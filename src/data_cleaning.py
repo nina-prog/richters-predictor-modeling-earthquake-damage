7dd205d18d0afcd7930cd92b7159686d28a5d960
nina-m-m <65343815+nina-prog@users.noreply.github.com>
2023-05-02 21:12:34 +0200

    Improve the function of the handling of the outliers to fit pipeline and code formatting

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,9 +1,14 @@
+"""
+This python file contains all functionality for the data cleaning
+"""
+
 # Imports
 import numpy as np
 import pandas as pd
 from sklearn.metrics import matthews_corrcoef
 from sklearn import preprocessing
 from scipy.stats import chi2_contingency
+from pandas.api.types import is_numeric_dtype
 
 
 def get_pearson_correlated_features(data=None, threshold=0.7):
@@ -26,7 +31,7 @@ def get_pearson_correlated_features(data=None, threshold=0.7):
             if abs(corr_matrix.iloc[i, j]) > threshold:
                 colname = corr_matrix.columns[i]
                 correlated_features.add(colname)
-    
+
     return correlated_features
 
 
@@ -46,33 +51,33 @@ def get_cramers_v_correlated_features(data=None, threshold=0.7):
     """
     # Encode features
     label = preprocessing.LabelEncoder()
-    data_encoded = pd.DataFrame() 
+    data_encoded = pd.DataFrame()
 
-    for i in data.columns :
-        data_encoded[i]=label.fit_transform(data[i])
+    for i in data.columns:
+        data_encoded[i] = label.fit_transform(data[i])
 
     # Internal function to calculate cramers V for two features
-    def _cramers_V(var1, var2) :
-        crosstab = np.array(pd.crosstab(var1,var2, rownames=None, colnames=None))  # Cross table building
+    def _cramers_V(var1, var2):
+        crosstab = np.array(pd.crosstab(var1, var2, rownames=None, colnames=None))  # Cross table building
         stat = chi2_contingency(crosstab)[0]  # Keeping of the test statistic of the Chi2 test
         obs = np.sum(crosstab)  # Number of observations
         mini = min(crosstab.shape) - 1  # Take the minimum value between the columns and the rows of the cross table
         return (stat / (obs * mini))
-        #return stat
+        # return stat
 
     # Calculate values for each pair of features
-    rows= []
+    rows = []
     for var1 in data_encoded:
         col = []
-        for var2 in data_encoded :
+        for var2 in data_encoded:
             cramers = _cramers_V(data_encoded[var1], data_encoded[var2])  # Cramer's V test
             col.append(round(cramers, 4))  # Keeping of the rounded value of the Cramer's V  
         rows.append(col)
-    
+
     # Create a pandas df from the results
     cramers_results = np.array(rows)
-    corr_matrix = pd.DataFrame(cramers_results, columns = data_encoded.columns, index =data_encoded.columns)
-    
+    corr_matrix = pd.DataFrame(cramers_results, columns=data_encoded.columns, index=data_encoded.columns)
+
     # Get the set of correlated features
     correlated_features = set()
     for i in range(len(corr_matrix.columns)):
@@ -80,7 +85,7 @@ def get_cramers_v_correlated_features(data=None, threshold=0.7):
             if abs(corr_matrix.iloc[i, j]) > threshold:
                 colname = corr_matrix.columns[i]
                 correlated_features.add(colname)
-    
+
     return correlated_features
 
 
@@ -88,7 +93,7 @@ def get_mcc_correlated_features(data=None, threshold=0.7):
     """
     Calculates the MCC correlation of all features and returns a set of features with a correlation greater than
     the threshold. Cramers V is based on Chi square, for reference see: https://en.wikipedia.org/wiki/Cram%C3%A9r%27s_V
-    Note that this function is desined to work for categorical features only!
+    Note that this function is designed to work for categorical features only!
     Code was copied and modified from this source:
     https://www.kaggle.com/code/chrisbss1/cramer-s-v-correlation-matrix/notebook
     
@@ -99,30 +104,30 @@ def get_mcc_correlated_features(data=None, threshold=0.7):
     """
     # Encode features
     label = preprocessing.LabelEncoder()
-    data_encoded = pd.DataFrame() 
+    data_encoded = pd.DataFrame()
 
     label = preprocessing.LabelEncoder()
-    data_encoded = pd.DataFrame() 
-    
+    data_encoded = pd.DataFrame()
+
     for c in data.columns:
         if c in data.columns:
             data_encoded[c] = label.fit_transform(data[c])
         else:
             data_encoded[c] = data[c]
-    
+
     # Calculate values for each pair of features
-    rows= []
+    rows = []
     for var1 in data_encoded:
         col = []
-        for var2 in data_encoded :
-            phi = matthews_corrcoef(data_encoded[var1], data_encoded[var2])  
+        for var2 in data_encoded:
+            phi = matthews_corrcoef(data_encoded[var1], data_encoded[var2])
             col.append(phi)  # phi  
         rows.append(col)
-    
+
     # Create a pandas df from the results
     phi_results = np.array(rows)
     corr_matrix = pd.DataFrame(phi_results, columns=data_encoded.columns, index=data_encoded.columns)
-    
+
     # Get the set of correlated features
     correlated_features = set()
     for i in range(len(corr_matrix.columns)):
@@ -130,7 +135,7 @@ def get_mcc_correlated_features(data=None, threshold=0.7):
             if abs(corr_matrix.iloc[i, j]) > threshold:
                 colname = corr_matrix.columns[i]
                 correlated_features.add(colname)
-    
+
     return correlated_features
 
 
@@ -172,14 +177,15 @@ def drop_correlated_features(data=None, config=None):
             features_to_drop = get_cramers_v_correlated_features(data=data[feature_names], threshold=d['threshold'])
         elif d['method'] == 'Pearson':
             features_to_drop = get_pearson_correlated_features(data=data[feature_names], threshold=d['threshold'])
-        else: 
+        else:
             print(f"Correlation method '{d['method']}' is not implemented.")
-        
+
         # Drop features
         if len(features_to_drop) > 0:
             data = data.drop(features_to_drop, axis=1)
-    
+
     return data
+
 
 def format_dtypes(df):
     """
@@ -197,61 +203,71 @@ def format_dtypes(df):
 
     return df
 
+
 def handle_outliers_IQR(df, ignore_cols, iqr_factor=1.5, method="replace"):
     """
     Handle outliers in a mixed categorical and numerical dataframe using the IQR method. Note that this function
-    only drops outliers based on IQR and does not consider any categorical features. It is assumed that any categorical
-    features in the dataframe will not contribute to the detection of outliers.
+     only drops outliers based on IQR and does not consider any categorical features. It is assumed that any categorical
+     features in the dataframe will not contribute to the detection of outliers.
 
     :param df: The input dataframe.
     :type df: pd.DataFrame
+    :param ignore_cols: The list of columns to ignore.
+    :type ignore_cols: list(str)
     :param iqr_factor: The factor to be multiplied with the IQR to determine the outlier bounds.
-    :type iqr_factor: float, optional (default=1.5)
+    :type iqr_factor: float,optional(default=1.5)
     :param method: The method to handle outliers. The options are:
-        - "drop": drops the rows with outliers.
-        - "cap": replaces the outliers with the upper or lower limit.
-    :type method: str, optional (default="drop")
+        -"hard_drop": drops the rows with outliers.
+        -"soft_drop": drops the rows where more than 30% of the row values are outliers.
+        -"replace": replaces the outliers with the upper or lower limit.
+    :type method: str,optional(default="drop")
+
     :return: The processed dataframe with outliers handled.
     :rtype: pd.DataFrame
     """
     # Identify the numerical columns in the dataframe
-    df = df.drop(ignore_cols, axis=1)
-    numeric_cols = df.select_dtypes(include=np.number).columns.tolist()
+    tmp = df[[x for x in df.columns if (x not in ignore_cols and is_numeric_dtype(df[x]))]]
 
     # Calculate the IQR for each numerical column
-    Q1 = df[numeric_cols].quantile(0.25)
-    Q3 = df[numeric_cols].quantile(0.75)
+    Q1 = tmp.quantile(0.25)
+    Q3 = tmp.quantile(0.75)
     IQR = Q3 - Q1
 
     # Calculate the lower and upper bounds for outliers
-    lower_bound = Q1 - iqr_factor * IQR
-    upper_bound = Q3 + iqr_factor * IQR
+    lower_bound = Q1 - (iqr_factor * IQR)
+    upper_bound = Q3 + (iqr_factor * IQR)
 
     # Identify the rows where any numerical value is outside the bounds aka identify the outliers
-    outlier_mask = (df[numeric_cols] < lower_bound) | (df[numeric_cols] > upper_bound)
+    outlier_mask = (tmp < lower_bound) | (tmp > upper_bound)
     outlier_rows = outlier_mask.any(axis=1)
 
-    print(f"Found {outlier_mask.count().sum()} outliers, method '{method}'")
+    print(
+        f"Found {outlier_mask.sum().sum()} outliers,using method'{method}'to handle them:" + "\n" + "#" * 10 +
+        "Count per column:" + "#" * 10 + "\n" + f"{outlier_mask.sum()}"
+    )
 
     if method == "hard_drop":
         # Drop rows with outliers
         df = df[~outlier_rows]
+
     elif method == "soft_drop":
         # Compute the number of outliers per row and drop rows based on the threshold (30%)
         count_outliers = outlier_mask.notna().sum(axis=1)
-        drop_indices = count_outliers[count_outliers >= 30 * len(numeric_cols)].index
+        drop_indices = count_outliers[count_outliers >= 30 * len(tmp.columns)].index
         df = df.drop(drop_indices)
+
     elif method == "replace":
-        upper_limit = df[numeric_cols].mean() + 3 * df[numeric_cols].std()
-        lower_limit = df[numeric_cols].mean() - 3 * df[numeric_cols].std()
+        upper_limit = tmp.mean() + 3 * tmp.std()
+        lower_limit = tmp.mean() - 3 * tmp.std()
         # Replace outliers with upper or lower limit
-        df[numeric_cols] = np.where(df[numeric_cols] > upper_limit, upper_limit,
-                                    np.where(df[numeric_cols] < lower_limit, lower_limit,
-                                             df[numeric_cols]))
+        tmp = tmp.clip(lower=lower_limit, upper=upper_limit, axis=1)
+        df = pd.concat([df, tmp], axis=1)
+
     else:
-        raise ValueError(f"Invalid method: {method}, must be either 'drop' or 'replace'.")
+        raise ValueError(f"Invalid method:{method},must be either 'hard_drop' or 'replace' or 'soft_drop'.")
 
     return df
+
 
 def prepare_data(df, config, ignore_cols, outlier_method):
     """
@@ -268,6 +284,7 @@ def prepare_data(df, config, ignore_cols, outlier_method):
         - "drop": drops the rows with outliers.
         - "cap": replaces the outliers with the upper or lower limit.
     :type outlier_method: str
+
     :return: cleaned pandas DataFrame
     :rtype: pandas.DataFrame
     """
@@ -280,6 +297,7 @@ def prepare_data(df, config, ignore_cols, outlier_method):
     df = handle_outliers_IQR(df, ignore_cols=ignore_cols, method=outlier_method)
 
     return df
+
 
 def group_categorical_features(df: pd.DataFrame, default_val: str = "others", verbose: bool = False) -> pd.DataFrame:
     """

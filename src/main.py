4675419a7124798e5997835bea2575109bdfa1cd
negralessio <Alessio.Negrini@live.de>
2023-05-08 11:12:17 +0200

    Fixed wrong returns in feature selection

--- src/main.py
+++ src/main.py
@@ -120,20 +120,22 @@ if not cfg["feature_engineering"]["feature_selection"]["skip"]:
                                                             scoring="matthews_corrcoef",
                                                             step=cfg["feature_engineering"]["feature_selection"]["step"],
                                                             verbose=0)
+        plot_rfecv_scoring(rfecv)
+
     if cfg["feature_engineering"]["feature_selection"]["method"] == "RFE":
         print("Selecting best features using RFE...")
-        best_feats, rfecv = get_top_k_features_using_rfe(x_train=train_data_cleaned,
+        best_feats, rfe = get_top_k_features_using_rfe(x_train=train_data_cleaned,
                                                          y_train=train_labels,
                                                          k=cfg["feature_engineering"]["feature_selection"]["k"], 
                                                          step=cfg["feature_engineering"]["feature_selection"]["step"], 
                                                          verbose=0)
+
     if cfg["feature_engineering"]["feature_selection"]["method"] == "MI":
         print("Selecting best features using MI...")
-        best_feats, rfecv = get_top_k_features_using_mi(x_train=train_data_cleaned,
+        best_feats, mi_scores = get_top_k_features_using_mi(x_train=train_data_cleaned,
                                                         y_train=train_labels,
                                                         k=cfg["feature_engineering"]["feature_selection"]["k"])
 
-    #plot_rfecv_scoring(rfecv)
     #print(f"*** Number of best selected features: {rfecv.n_features_} of {rfecv.n_features_in_} in total ***")
     print(f"\nSelected feature set: {best_feats}\n")
 
